56ff279ab43dfe0a8b82a5dd0654eb744e55d74a
iQuerz <niki.tenis@gmail.com>
2023-01-19 20:13:42 +0100

    Meeting with piksi changes

--- App/RTOG.App/RTOG.App/Hubs/LobbyHub.cs
+++ App/RTOG.App/RTOG.App/Hubs/LobbyHub.cs
@@ -5,9 +5,24 @@ namespace RTOG.App.Hubs
 {
     public class LobbyHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task updateLobbyPlayers(string lobbyID)
         {
             await Clients.All.SendAsync("updateLobby", lobbyID);
+        }
+
+        public async Task startGame(string gameID)
+        {
+            await Clients.All.SendAsync("startGame", gameID);
         }
     }
 }
